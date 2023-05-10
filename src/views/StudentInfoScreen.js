1ba1d77a7c2bb0eb34d1570bd7b9f8827ba4758c
MementoMori2318 <johnrogerargarin@gmail.com>
2023-05-10 16:58:41 +0800

    wew

--- src/views/StudentInfoScreen.js
+++ src/views/StudentInfoScreen.js
@@ -18,17 +18,30 @@ export default function StudentInfoScreen({ navigation }) {
     const [isMale, setisMale] = React.useState(0);
 
     const [isDisabledAdd, setDisableAdd] = useState(false);
+    const [isDisabledEdit, setDisableEdit] = useState(false);
+
+    React.useState(() => {
+      const clear = navigation.addListener("focus", () =>{
+        setDisableAdd(false);
+        setDisableEdit(true);
+        setStudentNo("");
+        setFirstName("");
+        setMiddleName("");
+        setLastName("");
+        setDateOfBirth("");
+        setisMale(0);
+      });
+      return clear;
+    },[navigation]);
 
     const getStudentInfo = (id) => {
-      if (id) {
         fetch("http://192.168.1.3/IT2C_Argarin/Api/student/" + id, {
           method: 'GET',
         })
         .then((res) => res.json())
         .then((result) => {
           console.log(result);
-          setDisableAdd(true);
-    
+          setDisableAdd(true); 
           if (result.data.student_info !== null) {
             setStudentNo(result.data.student_info.StudentNo);
             setFirstName(result.data.student_info.FirstName);
@@ -38,19 +51,16 @@ export default function StudentInfoScreen({ navigation }) {
             setDateOfBirth(result.data.student_info.DateOfBirth);
           }
         });
-      }
     };
     
-    useEffect(() => {
-      const focused = navigation.addListener("focus", () => {
-        getStudentInfo(route.params?.id);
-      });
-      return () => {
-        focused.remove();
-      };
-    }, [navigation, route.params?.id]);
-    
-    
+    useEffect(() =>{
+      (async () =>{
+        if(route.params !== undefined){
+          getStudentInfo(route.params.id);
+        }
+      })();
+    }, []);
+
     const postStudentInfo = () => {
         console.log("StudentNo = " + StudentNo);
         console.log("FirstName = " + FirstName);
@@ -90,7 +100,37 @@ export default function StudentInfoScreen({ navigation }) {
         }
       };
       
-
+      const putStudentInfo = () => {
+        if (StudentNo && FirstName && MiddleName && LastName && DateOfBirth) {
+          setDisableEdit(true);
+      
+          let formData = new URLSearchParams();
+            formData.append("SN", StudentNo);
+            formData.append("FN", FirstName);
+            formData.append("MN", MiddleName);
+            formData.append("LN", LastName);
+            formData.append("Sex", isMale);
+            formData.append("DOB", DateOfBirth);
+          fetch("http://192.168.1.3/IT2C_Argarin/Api/student", {
+            method: 'PUT',
+            body: formData.toString,
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              if (data.meta.code == 200) {
+                navigation.navigate("Student");
+                setStudentNo("");
+                setFirstName("");
+                setMiddleName("");
+                setLastName("");
+                setisMale(0);
+                setDateOfBirth("");
+              }
+              setDisableEdit(false);
+            });
+        }
+      };
     return (
         <>
           <FlatList
@@ -185,6 +225,18 @@ export default function StudentInfoScreen({ navigation }) {
                         borderRadius: 8,
                     }}
                     icon={<Icon name="chevron-left" type="font-awesome" color="white"/>}
+                  />
+                   <Button
+                    onPress={putStudentInfo}
+                    title="Edit Student"
+                    type="solid"
+                    disabled={isDisabledEdit}
+                    containerStyle={{
+                        marginHorizontal: 16,
+                        marginVertical: 8,
+                        borderRadius: 8,
+                    }}
+                    icon={<Icon name="pencil" type="font-awesome" color="white"/>}
                   />
                 </View>
               </>
